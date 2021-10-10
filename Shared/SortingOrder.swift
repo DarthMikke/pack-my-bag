daf628b667dc537cda6f1ca83b7268aafba07489
Michal Jan Warecki <mwarecki1@gmail.com>
2021-10-10 13:38:48 +0200

    Enum SortingOrder in its own file; protocol SortableObject

---
+++ Shared/SortingOrder.swift
@@ -0,0 +1,29 @@
+//
+//  SortingOrder.swift
+//  Pack my Bag
+//
+//  Created by Michal Jan Warecki on 09/10/2021.
+//
+
+import Foundation
+
+public enum SortingOrder: String, CaseIterable {
+    case createdNto, createdOtn, modifiedNto, modifiedOtn, AZ, ZA
+    
+    var long: String {
+        switch self {
+        case .createdOtn:
+            return "Created, oldest to newest"
+        case .createdNto:
+            return "Created, newest to oldest"
+        case .modifiedOtn:
+            return "Modified, oldest to newest"
+        case .modifiedNto:
+            return "Modified, newest to oldest"
+        case .AZ:
+            return "A-Z"
+        case .ZA:
+            return "Z-A"
+        }
+    }
+}
